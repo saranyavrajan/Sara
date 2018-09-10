e85bf12f637617f07b5fdae77bfa0f3b9fed3cf9
emailgowrisankar@gmail.com <Bendix@89>
2018-09-10 13:21:54 +0530

    Create and Login

--- src/main/java/testcases/TC001_Login.java
+++ src/main/java/testcases/TC001_Login.java
@@ -10,8 +10,8 @@ public class TC001_Login extends SeMethods{
 	public void login() {
 		startApp("chrome", "http://leaftaps.com/opentaps");
 		WebElement eleUserName = locateElement("id", "username");
-		//type(eleUserName, "DemoSalesManager");
-		type(eleUserName, "123");
+		type(eleUserName, "DemoSalesManager");
+		//type(eleUserName, "123");
 		WebElement elePassword = locateElement("id","password");
 		type(elePassword, "crmsfa");
 		WebElement eleLogin = locateElement("class","decorativeSubmit");
